20b06ef1083b716f19e311be541a09f407631462
AYKUT GUNDOGDU <aykutgundogdu33@gmail.com>
2022-03-20 00:15:25 +0100

    quete node.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const package = require('object')
+var cowsay = require("cowsay");
+
+console.log(cowsay.say({
+    texte : " Aykut du campus de Bordeaux",
+    e : "oO",
+    T : "U"
+}));
---
+++ information.js
@@ -0,0 +1,6 @@
+const objet = {
+    name : aykut ,
+    campus : bordeaux ,
+}
+
+module.exports = object;
